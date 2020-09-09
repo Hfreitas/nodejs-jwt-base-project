62fa1b25864c1fb8e9c2a1607285a18fc0ce3ad9
Hfreitas <hfreitas86@gmail.com>
2020-09-09 11:01:39 -0300

    resolvendo warning

--- models/user.js
+++ models/user.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const { DB_URL } = process.env;
 
-mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(DB_URL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const UserSchema = new mongoose.Schema({
   username: {
